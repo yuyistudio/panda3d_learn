64c6f1a6883b31095ef0c97e935b3ff2ff1b7dba
liyong <liyong@bytedance.com>
2018-01-16 16:52:48 +0800

    测试mazelib
    Change-Id: Idcd49f26387c12656782aae9178b442d81fa6ad0

--- tools/mg_util/maze_lib_test.py
+++ tools/mg_util/maze_lib_test.py
@@ -1,10 +1,39 @@
 # encoding: utf8
 
-from mazelib.mazelib import *
+from mazelib import mazelib
+from mazelib.solve.WallFollower import WallFollower
 
-m = Maze()
-m.generator = Prims(50, 51)
-m.solver = WallFollower()
-print m.generate_monte_carlo(100, 10, 1.0)
+from mazelib.generate.BacktrackingGenerator import BacktrackingGenerator
+from mazelib.generate.BinaryTree import BinaryTree
+from mazelib.generate.CellularAutomaton import CellularAutomaton
+from mazelib.generate.Division import Division
+from mazelib.generate.Ellers import Ellers
+from mazelib.generate.GrowingTree import GrowingTree
+from mazelib.generate.HuntAndKill import HuntAndKill
+from mazelib.generate.Kruskal import Kruskal
+from mazelib.generate.Perturbation import Perturbation
+from mazelib.generate.Prims import Prims
+from mazelib.generate.Sidewinder import Sidewinder
+from mazelib.generate.TrivialMaze import TrivialMaze
+from mazelib.generate.Wilsons import Wilsons
 
 
+def gen_mazes():
+    m = mazelib.Maze()
+    generators = [BacktrackingGenerator, BinaryTree, CellularAutomaton, Division, Ellers, GrowingTree,
+                  HuntAndKill, Kruskal, Prims, Sidewinder, TrivialMaze, Wilsons]
+
+    m.solver = WallFollower()
+    for generator in generators:
+        print ('generating with %s' % generator)
+        m.generator = generator(9, 9)
+        try:
+            maze_list = m.generate_monte_carlo(2, 2, 1.0)
+        except RuntimeError, e:
+            print('failed to solve %s' % generator)
+            continue
+        maze = maze_list[0]
+        mazelib.print_grid(maze['grid'], maze['start'], maze['end'])
+
+
+gen_mazes()
